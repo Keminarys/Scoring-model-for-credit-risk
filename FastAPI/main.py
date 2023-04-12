1cb683bf89ee84c68a3716f4ee8adbcc2dbe34a2
Keminarys <115616113+Keminarys@users.noreply.github.com>
2023-04-12 15:52:51 +0200

    Update main.py

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,1 +1,19 @@
+from fastapi import FastAPI
+import uvicorn
+import joblib
+import pandas as pd
+import numpy as np
+import shap
+import json
 
+#Init
+app = FastAPI(debug=True)
+
+#Data 
+lgbm = joblib.load('LGBM_Thresh_04785.sav')
+data = joblib.load('data_api.pkl')
+list_ID = data.index.tolist()
+
+#Main
+if __name__ == '__main__' : 
+   uvicorn.run(app, host="127.0.0.1", port="8000")
