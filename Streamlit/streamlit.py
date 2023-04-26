087b118b1ede4232e1ff29a34721ef2c4f3db9c2
Keminarys <115616113+Keminarys@users.noreply.github.com>
2023-04-26 16:21:30 +0200

    Update streamlit.py

--- Streamlit/streamlit.py
+++ Streamlit/streamlit.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import numpy as np
+import shap
+import json
+import joblib
+import plotly.express as px
+import plotly.graph_objects as go
+import streamlit as st
 
+
+st.set_page_config(layout="wide")
+st.title('Home Credit Risk Application : Does the applicant can repay his loan ?')  
